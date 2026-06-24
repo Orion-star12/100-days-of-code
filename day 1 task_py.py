c49d933cf941345b7469384dffc3a3f49a2ca0aa
Orion-star12 <iboroemmanueletukakpan@gmail.com>
2026-06-24 19:25:28 +0100

    completed day 1

---
+++ day 1 task_py.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator")
+name = input("What is the name of the city you grew up in?\n")
+pet = input("What is the name of your favourite pet?\n")
+print("Your band name could be: " + name + " " + pet)
+
